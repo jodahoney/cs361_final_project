50a97eb500b8c55fcfd4528e33a442693555c279
Joseph Dehoney <josephdehoney@gmail.com>
2022-11-27 14:05:30 -0800

    added base files and db connector + env config

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hello, and welcome to my Final Project for CS361 at Oregon State University.
+
+### The fincal project is a daily calorie tracker / food diary.
+It allows you to:
+* Enter in the food you have eaten for the day
+* See the calories that are in each meal
+* Calcualte the total number of calories suggested to eat in one day.
---
+++ project/database/db_connector.py
@@ -0,0 +1,58 @@
+import MySQLdb
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Load our environment variables from the .env file in the root of our project.
+load_dotenv(find_dotenv())
+
+# Set the variables in our application with those environment variables
+host = os.environ.get("340DBHOST")
+user = os.environ.get("340DBUSER")
+passwd = os.environ.get("340DBPW")
+db = os.environ.get("340DB")
+
+def connect_to_database(host = host, user = user, passwd = passwd, db = db):
+    '''
+    connects to a database and returns a database objects
+    '''
+    db_connection = MySQLdb.connect(host,user,passwd,db)
+    return db_connection
+
+def execute_query(db_connection = None, query = None, query_params = ()):
+    '''
+    executes a given SQL query on the given db connection and returns a Cursor object
+
+    db_connection: a MySQLdb connection object created by connect_to_database()
+    query: string containing SQL query
+
+    returns: A Cursor object as specified at https://www.python.org/dev/peps/pep-0249/#cursor-objects.
+    You need to run .fetchall() or .fetchone() on that object to actually acccess the results.
+
+    '''
+
+    db_connection = connect_to_database()
+
+    if db_connection is None:
+        print("No connection to the database found! Have you called connect_to_database() first?")
+        return None
+
+    if query is None or len(query.strip()) == 0:
+        print("query is empty! Please pass a SQL query in query")
+        return None
+
+    print("Executing %s with %s" % (query, query_params));
+    
+    cursor = db_connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(query, query_params)
+    db_connection.commit()
+    return cursor
+
+if __name__ == '__main__':
+    print("Executing a sample query on the database using the credentials from db_credentials.py")
+    db = connect_to_database()
+    query = "SELECT * from Flights;"
+    results = execute_query(db, query)
+    print("Printing results of %s" % query)
+
+    for r in results.fetchall():
+        print(r)
